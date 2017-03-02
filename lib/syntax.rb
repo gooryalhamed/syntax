07d525ff95265b67b861c4f7d4c9fd2e14caf9a7
Goory al Hamed <gooryalhamed@hotmail.com>
2017-03-02 21:57:45 +0300

    initial thought

---
+++ lib/syntax.rb
@@ -0,0 +1,1 @@
+require_relative "syntax/cli.rb"
---
+++ lib/syntax/cli.rb
@@ -0,0 +1,12 @@
+class Cli
+	def home
+		puts "Categories"
+		puts "=========="
+		puts "1. HTML    2. CSS    3. JS"
+		puts "4. Github  5. Ruby    6. Rails"
+		puts "select number to start or enter e for exit"
+		puts "-------------------------------"
+		puts ""
+		puts "Which category would you like to serach in?"
+	end
+end
